8976d2649b43c51ac9842e0e88b6f8c0b271008b
Cruise Brantley <cruise.brantley@gmail.com>
2018-12-02 02:19:45 -0600

    Fixed styling for popup, commented checkbox code

--- popup.js
+++ popup.js
@@ -1,36 +1,32 @@
-// let changeColor = document.getElementById("changeColor");
-
-// chrome.storage.sync.get("color", data => {
-//   changeColor.style.backgroundColor = data.color;
-//   changeColor.setAttribute("value", data.color);
-// });
-
 let objectStore = [];
 let page = document.getElementById("seriesDiv");
 
 chrome.storage.sync.get("objectStore", data => {
   objectStore = data.objectStore;
-  // console.log(objectStore);
   for (let item of objectStore) {
-    let series = document.createElement("span");
-    // series.style.backgroundColor = item;
+    let series;
+    item.watched
+      ? (series = document.createElement("strike"))
+      : (series = document.createElement("span"));
+
+    // var newCheckBox = document.createElement("input");
+    // newCheckBox.type = "checkbox";
+    // newCheckBox.checked = item.watched;
+
+    linebreak = document.createElement("br");
+
+    item.watched
+      ? (series.style.color = "red")
+      : (series.style.color = "black");
     series.innerHTML = item.title;
-    console.log(item.title);
-    series.addEventListener("click", function() {
-      // chrome.storage.sync.set({ color: item }, function() {
-      //   console.log("color is " + item);
-      // });
-    });
     page.appendChild(series);
+    // page.appendChild(newCheckBox);
+    page.appendChild(linebreak);
+
+    // series.addEventListener("click", function() {
+    //   chrome.storage.sync.set({ color: item }, function() {
+    //     console.log("color is " + item);
+    //   });
+    // });
   }
 });
-
-// changeColor.onclick = element => {
-//   console.log(objectStore);
-//   let color = element.target.value;
-//   chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-//     chrome.tabs.executeScript(tabs[0].id, {
-//       code: 'document.body.style.backgroundColor = "' + color + '";'
-//     });
-//   });
-// };
