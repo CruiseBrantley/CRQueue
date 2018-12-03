7475970e12a48e692c1ee6e9547286750ec6f695
Cruise Brantley <cruise.brantley@gmail.com>
2018-12-03 00:41:15 -0600

    Lots of code for expanding description div

--- contentscript.js
+++ contentscript.js
@@ -17,9 +17,31 @@ for (let i = 0; i < progressList.length; i++) {
 newerList.forEach((item, index) => (objectStore[index].title = item));
 
 let currentEpisodeArray = document.querySelectorAll(".landscape-element.left");
+//Current Episode URLs
 currentEpisodeArray.forEach(
   (item, index) => (objectStore[index].episodeURL = item.href)
-);
+); //added to objectStore
+
+let episodeDataContainer = document.querySelectorAll(
+  ".series-data.ellipsis:not(.block)"
+); //episode number and title text string
+let regexp = /(?:Next up: )(?:Episode )(\S+)(?:\ \–\ )?(.+) */;
+//regex to break the string down
+let noNumberRegexp = /(?:Next up: )(.+) */;
+//regex without episode number
+episodeDataContainer.forEach((data, index) => {
+  let extractedArray = regexp.exec(data.innerText);
+  if (extractedArray !== null) {
+    objectStore[index].episodeNumber = extractedArray[1];
+    objectStore[index].episodeDescription = extractedArray[2];
+    //case with episode number
+  } else {
+    extractedArray = noNumberRegexp.exec(data.innerText);
+    objectStore[index].episodeNumber = null;
+    objectStore[index].episodeDescription = extractedArray[1];
+    //case of no episode number
+  }
+}); //added to objectStore
 //objectStore complete
 
 chrome.storage.local.set({ objectStore }, function() {
--- popup.js
+++ popup.js
@@ -4,33 +4,69 @@ let page = document.getElementById("seriesDiv");
 chrome.storage.local.get("objectStore", data => {
   if (data.objectStore) {
     objectStore = data.objectStore;
+    console.log(objectStore);
     let count = 0;
     for (let item of objectStore) {
+      let linebreak = document.createElement("br");
       let container = document.createElement("div");
       container.style.width = "100%";
       !(count % 2)
         ? (container.style.background = "rgba(252,181,2,.44)")
         : null;
       container.style.marginBottom = "2px";
+      container.style.userSelect = "none";
+      container.style.transitionDuration = ".25s";
+
+      let episodeNumber = document.createElement("span");
+      episodeNumber.innerHTML =
+        "Episode: " + (item.episodeNumber ? item.episodeNumber : "Special");
+      episodeNumber.style.marginBottom = "5px";
+      episodeNumber.style.padding = "5px";
+      episodeNumber.style.display = "inline-block";
+      episodeNumber.style.fontSize = "1rem";
+
+      let descriptionContainer = document.createElement("div");
+      descriptionContainer.style.display = "flex";
+      descriptionContainer.style.alignItems = "center";
+      descriptionContainer.style.height = "0px";
+      descriptionContainer.style.transitionDuration = ".25s";
+      descriptionContainer.style.overflow = "hidden";
+
+      let episodeDescription = document.createElement("span");
+      episodeDescription.innerHTML = item.episodeDescription;
+      episodeDescription.style.marginBottom = "5px";
+      episodeDescription.style.padding = "5px";
+      episodeDescription.style.fontSize = "1rem";
+
       let series = document.createElement("span");
-
-      linebreak = document.createElement("br");
-
       item.watched ? (series.style.textDecoration = "line-through") : null;
       series.innerHTML = item.title;
       series.style.marginBottom = "5px";
       series.style.padding = "5px";
       series.style.display = "inline-block";
+      series.style.fontSize = "1.2rem";
       series.style.cursor = "pointer";
       series.style.transitionDuration = ".25s";
-      series.style.fontSize = "1rem";
       series.style.border = "1px solid transparent";
+      series.style.maxWidth = "300px";
 
-      series.addEventListener("click", () => {
+      series.addEventListener("click", e => {
+        e.stopPropagation();
         chrome.tabs.update({
           url: item.episodeURL
         });
       });
+
+      let expanded = false;
+      container.addEventListener("click", () => {
+        expanded = !expanded;
+        if (expanded) {
+          descriptionContainer.style.height = "100px";
+        } else {
+          descriptionContainer.style.height = "0";
+        }
+      }); //Expand description logic
+
       container.addEventListener("mouseover", () => {
         series.style.boxShadow = "-7px 7px orange";
         series.style.border = "1px solid black";
@@ -39,16 +75,21 @@ chrome.storage.local.get("objectStore", data => {
         series.style.background = "white";
       });
       container.addEventListener("mouseleave", () => {
-        series.style.boxShadow = null;
-        series.style.border = "1px solid transparent";
-        series.style.marginLeft = null;
-        item.watched ? (series.style.textDecoration = "line-through") : null;
-        series.style.background = null;
+        if (!expanded) {
+          series.style.boxShadow = null;
+          series.style.border = "1px solid transparent";
+          series.style.marginLeft = null;
+          item.watched ? (series.style.textDecoration = "line-through") : null;
+          series.style.background = null;
+        }
       });
 
       page.appendChild(container);
       container.appendChild(series);
       container.appendChild(linebreak);
+      container.appendChild(descriptionContainer);
+      descriptionContainer.appendChild(episodeNumber);
+      descriptionContainer.appendChild(episodeDescription);
       count++;
     }
   }
