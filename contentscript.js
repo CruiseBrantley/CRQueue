13550fe36095dd0a4477f6fbfc3ae72c1b44a69a
Cruise Brantley <cruise.brantley@gmail.com>
2020-01-24 18:49:09 -0600

    Fixed scraper to match Crunchyroll changes

--- contentscript.js
+++ contentscript.js
@@ -1,20 +1,20 @@
-let list = document.querySelectorAll(".queue-wrapper.cf");
+let list = document.querySelectorAll(".queue-item-wrapper");
 let regexp = /(.+\n)(?:.+:)(?: Episode (.+) – )?(.+)?(?:\n+)?(.+)?/;
 let objectStore = [];
 
 for (let i = 0; i < list.length; i++) {
   const currentObject = {};
-  const infoArray = regexp.exec(list[i].children[2].innerText);
+  const infoArray = regexp.exec(list[i].children[0].children[2].innerText);
   currentObject.title = infoArray[1];
   currentObject.episodeNumber = infoArray[2];
   currentObject.episodeTitle = infoArray[3];
   infoArray[4]
     ? (currentObject.episodeDescription = infoArray[4])
     : (currentObject.episodeDescription = "No episode description available.");
-  currentObject.episodeURL = list[i].children[2].href;
-  currentObject.episodeImage = list[i].children[2].children[0].children[0].src;
+  currentObject.episodeURL = list[i].children[0].children[2].href;
+  currentObject.episodeImage = list[i].children[0].children[2].children[0].children[0].children[0].src;
   const episodeProgress =
-    list[i].children[2].children[0].children[1].children[0].style.width;
+    list[i].children[0].children[2].children[0].children[1].children[0].style.width;
   episodeProgress.slice(0, -1) > 90
     ? (currentObject.watched = true)
     : (currentObject.watched = false);
